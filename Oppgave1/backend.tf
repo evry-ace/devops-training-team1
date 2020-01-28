c96709d8ed12f8761346f9b2b06089eaf0468dd3
Knut Ivar Alvestad <knut.alvestad@evry.com>
2020-01-28 13:33:40 +0100

    fix securety stuff

--- Oppgave1/backend.tf
+++ Oppgave1/backend.tf
@@ -4,8 +4,5 @@ terraform {
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
 
-    # rather than defining this inline, the Access Key can also be sourced
-    # from an Environment Variable - more information is available below.
-    access_key = "xynhek189qVKUtKiClvD4JlDneOCmiKwvTYDDz6JhNZCgnTNBfKxtx3weT166CIgVfg8m5rQ7g1/krl4kNKDPA=="
   }
 }
