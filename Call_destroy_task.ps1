d884bea067d276b74a63e6bdca1a124f6b1ac67d
Knut Ivar Alvestad <knut.alvestad@evry.com>
2020-02-10 14:55:08 +0100

    Add ps script to call terraform destroy

---
+++ Call_destroy_task.ps1
@@ -0,0 +1,10 @@
+﻿$body = @{
+ "event_type"="do-something"
+} | ConvertTo-Json
+
+$header = @{
+ "Accept"="application/vnd.github.everest-preview+json"
+ "Authorization"="token e24b4a689f33e7898018c071e488b3578d2f167b"
+} 
+
+Invoke-RestMethod -Uri "https://api.github.com/repos/evry-ace/devops-training-team1/dispatches" -Method 'Post' -Body $body -Headers $header | ConvertTo-HTML
