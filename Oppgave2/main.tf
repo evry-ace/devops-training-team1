3fe749bb3913a862c9ff6af3f8fd24614af469a0
Kjetil <kjetil.gynnild@tietoevry.com>
2020-03-18 13:27:39 +0100

    Part 2.2 - Add Nginx

--- Oppgave2/main.tf
+++ Oppgave2/main.tf
@@ -45,3 +45,55 @@ resource "azurerm_kubernetes_cluster" "example" {
   }
 }
 
+resource "kubernetes_pod" "nginxpod" {
+  metadata {
+    name = "nginxpod"
+    labels = {
+      app.kubernetes.io / name = "Mynginxapp"
+    }
+  }
+
+  spec {
+    container {
+      image = "nginx:1.7.9"
+      name  = "example"
+
+      env {
+        name  = "environment"
+        value = "nginxtest"
+      }
+
+      liveness_probe {
+        http_get {
+          path = "/nginx_status"
+          port = 80
+
+          http_header {
+            name  = "X-Custom-Header"
+            value = "Awesome"
+          }
+        }
+
+        initial_delay_seconds = 3
+        period_seconds        = 3
+      }
+    }
+
+    dns_config {
+      nameservers = ["1.1.1.1", "8.8.8.8", "9.9.9.9"]
+      searches    = ["example.com"]
+
+      option {
+        name  = "ndots"
+        value = 1
+      }
+
+      option {
+        name = "use-vc"
+      }
+    }
+
+    dns_policy = "None"
+  }
+}
+
