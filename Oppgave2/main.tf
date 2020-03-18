7dd044ff05f2e4ce2f2940665b386871f78a3398
Knut Ivar Alvestad <knut.alvestad@evry.com>
2020-03-18 10:36:11 +0100

    clean variables and add otput file

--- Oppgave2/main.tf
+++ Oppgave2/main.tf
@@ -3,9 +3,39 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_resource_group" "example" {
+  name = "devops-training-team1"
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
-  location            = "${var.location}"
-  resource_group_name = var.resource_group
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
 }
+
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks1"
+  kubernetes_version  = "1.15.10"
+
+  default_node_pool {
+    name           = "default"
+    node_count     = 1
+    vm_size        = "Standard_D2_v2"
+    vnet_subnet_id = azurerm_virtual_network.vnet.id
+  }
+
+  service_principal {
+    client_id     = var.clId
+    client_secret = var.clSec
+  }
+
+  tags = {
+    Environment = "part1"
+  }
+}
+
