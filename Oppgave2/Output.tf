5eae5ba342764cd994d484cb268b3a012a094d98
Knut Ivar Alvestad <knut.alvestad@evry.com>
2020-03-19 10:40:35 +0100

    New name for output

--- Oppgave2/Output.tf
+++ Oppgave2/Output.tf
@@ -10,6 +10,6 @@ output "lb_ip" {
   value = kubernetes_service.nginx.load_balancer_ingress[0].ip
 }
 
-output "lb_ip" {
+output "lb_dp_ip" {
   value = kubernetes_service.scalablenginx.load_balancer_ingress[0].ip
 }
